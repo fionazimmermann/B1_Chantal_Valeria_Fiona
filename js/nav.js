5e1c76253297060341c3a43019c07e4f279873b1
valeriavondaeniken <132079787+valeriavondaeniken@users.noreply.github.com>
2023-06-07 17:07:05 +0200

    Delete js directory

--- js/nav.js
+++
@@ -1,5 +0,0 @@
-const toggleNavigation = (element) => {
-    document.getElementById("mobile-nav").classList.toggle("nav-visible");
-}
-
-document.getElementById("hamburger").addEventListener("click", toggleNavigation);
